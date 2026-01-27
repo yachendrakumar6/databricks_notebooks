508d63b2f6994bd9449dd0e74cf88734927b17b1
yachendrakumar6 <yachendrayadav@gmail.com>
2026-01-27 09:53:34 +0000

    changes made to conditional_logic file

--- Note Books/Python Scripts/Conditional & Logical/conditional_logical.py
+++ Note Books/Python Scripts/Conditional & Logical/conditional_logical.py
@@ -5,6 +5,9 @@
 # MAGIC when(condition, value).otherwise(value)
 # MAGIC when(cond1, val1).when(cond2, val2).otherwise(val3)
 # MAGIC Always add .otherwise() → avoids NULL surprises
+# MAGIC expr(CASE WHEN) 
+# MAGIC expr("ifnull(column, replacement)")
+# MAGIC
 # MAGIC
 
 # COMMAND ----------
@@ -31,20 +34,20 @@ display(df)
 
 # COMMAND ----------
 
-# df_when = df.withColumn("salary_band", 
-#                         when(col("salary") >= 80000, "High")
-#                         .when(col("salary") >= 50000, "Medium")
-#                         .otherwise("Low")
-#                         )
-# display(df_when)
+df_when = df.withColumn("salary_band", 
+                        when(col("salary") >= 80000, "High")
+                        .when(col("salary") >= 50000, "Medium")
+                        .otherwise("Low")
+                        )
+display(df_when)
 
-# df_when1 = df.withColumn(
-#     "bonus_eligible",
-#     when((col("dept") == "IT") & (col("salary") >= 60000), "eligible")
-#     .when((col("dept") == "HR") & (col("salary") >= 40000), "eligible")
-#     .otherwise("ineligible")
-# )
-# display(df_when1)
+df_when1 = df.withColumn(
+    "bonus_eligible",
+    when((col("dept") == "IT") & (col("salary") >= 60000), "eligible")
+    .when((col("dept") == "HR") & (col("salary") >= 40000), "eligible")
+    .otherwise("ineligible")
+)
+display(df_when1)
 
 df_when2 = df.withColumn(
     "salary_status",
@@ -52,3 +55,41 @@ df_when2 = df.withColumn(
     .otherwise("disclosed")
 )
 display(df_when2)
+
+df_expr = df.withColumn(
+    "grade",
+    expr("""
+        CASE 
+            WHEN salary >= 80000 THEN 'A'
+            WHEN salary >= 60000 THEN 'B'
+            ELSE 'C'
+        END
+    """)
+)
+display(df_expr)
+
+df_expr_ifnull = df.withColumn(
+    "salary_filled",
+    expr("ifnull(salary, 0)")
+)
+display(df_expr_ifnull)
+
+df.withColumn(
+    "salary_filled",
+    coalesce(col("salary"), lit(0))
+).display()
+
+df_coalsece = df.withColumn(
+    "final_status",
+    when(col("status").isNull(), "UNKNOWN")
+    .when(col("status") == "A", "ACTIVE")
+    .otherwise("INACTIVE")
+)
+display(df_coalsece)
+
+df_when_coalesce = df.withColumn(
+    "final_salary",
+    when(coalesce(col("bonus"), lit(0)) > 5000, "High")
+    .otherwise("Normal")
+)
+display(df_when_coalesce)
